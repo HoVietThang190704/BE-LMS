0265e62363b9993663f578c79f9ec88219f0a4e3
VietThang1907 <hovietthang1907@gmail.com>
2026-01-03 15:05:38 +0700

    NQ-143-Bổ xung api để Admin quản lý tiến độ của các khóa học

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -4,5 +4,6 @@ import { homeController } from '../presentation/controllers/HomeController';
 const router = Router();
 
 router.get('/', (req, res) => homeController.getDashboard(req, res));
+router.get('/courses', (req, res) => homeController.listCourses(req, res));
 
 export default router;
